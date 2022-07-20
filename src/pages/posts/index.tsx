ef08b9c06beb51b77f556225237df60b0bf27222
Albert Conceição <developer.albert@outlook.com>
2022-07-20 15:34:20 -0300

    subindo projeto

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -48,7 +48,7 @@ export default function Posts({ posts }:PostsProps) {
 export const getStaticProps: GetStaticProps = async () => {
     const prismic = getPrismicClient()
 
-    const response = await prismic.query<any>([
+    const response = await prismic.query([
         Prismic.predicates.at('document.type', 'publication')
     ], {
         fetch: ['publication.title', 'publication.content'],
